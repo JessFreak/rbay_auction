b8c58cf785c31a82f83ff9db9a02dbd6cd61ab7c
JessFreak <maliy.olexander@lll.kpi.ua>
2024-08-15 18:03:08 +0300

    added uniqueness to items views

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -7,5 +7,6 @@ export const usernamesKey = `usernames`;
 export const usernamesUniqueKey = `usernames:unique`;
 
 export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`;
 export const itemsByViewsKey = 'items:views';
 export const itemsByEndingAtKey = 'items:endingAt';
